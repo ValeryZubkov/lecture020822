b0c0e0a08d4f1e97100587ab7a5cfae92da28fdd
Valery Zubkov <bouddha@mail.ru>
2022-08-05 20:28:48 +0400

    add project with double

---
+++ Project_004/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+double numberA = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numberB = double.Parse(Console.ReadLine());
+double dif = numberA / numberB;
+Console.WriteLine("При делении Первого числа на Второе получим значение: " + dif);
