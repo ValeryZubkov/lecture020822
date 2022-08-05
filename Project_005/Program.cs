6e5027f9441aab7d250b929e38cc6b1613853784
Valery Zubkov <bouddha@mail.ru>
2022-08-05 21:02:03 +0400

    add project if-else

---
+++ Project_005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("Ура! Это Маша.");
+}
+else
+{
+Console.WriteLine("Привет, " + username);
+}
