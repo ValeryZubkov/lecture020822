4e4f85ace22aa614d501d92c63e9f53232db1657
Valery Zubkov <bouddha@mail.ru>
2022-08-05 21:17:09 +0400

    add project max

---
+++ Project_006/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+int b = 5;
+int c = 8;
+int d = 6;
+int e = 3;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;   
+if (e > max) max = e;
+Console.WriteLine("Максимальное число равно: " + max);
