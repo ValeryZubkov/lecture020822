86efd4be79a798fe6d6b9346f405a74a4ac1937c
Valery Zubkov <bouddha@mail.ru>
2022-08-05 20:10:42 +0400

    add project sum

---
+++ Project_003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+int sum = numberA + numberB;
+Console.WriteLine("Сумма Ваших чисел равна: " + sum);
